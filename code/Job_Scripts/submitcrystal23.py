96d9ae9df7e40c2341f82f830459af11005485aa
Lewis <107374486+Lewis0770@users.noreply.github.com>
2025-05-30 13:08:20 -0400

    Update submitcrystal23.py

--- code/Job_Scripts/submitcrystal23.py
+++ code/Job_Scripts/submitcrystal23.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This script is in place to try and determine the average bond length of carbons in a unit cell
+This script is in place to submit all .d12 input files in the current directory to CRYSTAL23
 """
 import os, sys, math
 import re
@@ -12,7 +12,7 @@ import itertools
 data_folder = os.getcwd() 
 data_files = os.listdir(data_folder)
 
-"""Reads each file given by data_folder and loops through to find the average bond length"""
+"""Reads each file given by data_folder and loops through to submit each one"""
 for file_name in data_files:
   if ".d12" in file_name:
     submit_name = file_name.split(".d12")[0]
