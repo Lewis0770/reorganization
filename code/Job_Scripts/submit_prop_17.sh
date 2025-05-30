16bc6660ddc94a2df0e6aa1abdcc966fe42b6513
Lewis <107374486+Lewis0770@users.noreply.github.com>
2025-05-30 13:02:36 -0400

    Update submit_prop_17.sh

--- code/Job_Scripts/submit_prop_17.sh
+++ code/Job_Scripts/submit_prop_17.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+"""
+This script generates and submits SLURM job scripts for running CRYSTAL17 Pproperties calculations.
+Takes a job name as argument, creates a .sh file with SLURM directives, and submits it to the queue.
+"""
+
 echo '#!/bin/bash --login' > $1.sh
 echo '#SBATCH -J '$1 >> $1.sh
 out=$1
@@ -18,18 +24,14 @@ echo '#SBATCH --mem=80G' >> $1.sh
 echo 'export JOB='$1 >> $1.sh
 echo 'export DIR=$SLURM_SUBMIT_DIR
 export scratch=$SCRATCH/crys17/prop
-
 echo "submit directory: "
 echo $SLURM_SUBMIT_DIR
-
 module purge
 module load CRYSTAL/17-intel-2023a
-
 mkdir  -p $scratch/$JOB
 cp $DIR/$JOB.d3  $scratch/$JOB/INPUT
 cp $DIR/$JOB.f9  $scratch/$JOB/fort.9
 cd $scratch/$JOB
-
 mpirun -n $SLURM_NTASKS Pproperties 2>&1 >& $DIR/${JOB}.out
 #srun Pproperties 2>&1 >& $DIR/${JOB}.out
 cp fort.9  ${DIR}/${JOB}.f9
@@ -42,5 +44,4 @@ cp SEEBECK.DAT ${DIR}/${JOB}.SEEBECK.DAT
 cp SIGMAS.DAT ${DIR}/${JOB}.SIGMAS.DAT
 cp KAPPA.DAT ${DIR}/${JOB}.KAPPA.DAT
 cp TDF.DAT ${DIR}/${JOB}.TDF.DAT' >> $1.sh
-
 sbatch $1.sh
