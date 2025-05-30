10c069929df818d12559b4d24901cfdc5914a5dd
Lewis <107374486+Lewis0770@users.noreply.github.com>
2025-05-30 12:56:39 -0400

    Update submit_prop.py

--- code/Job_Scripts/submit_prop.py
+++ code/Job_Scripts/submit_prop.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """
-This script is in place to try and determine the average bond length of carbons in a unit cell
+This script finds all .d3 files in the current directory and submits them for processing 
+by running submit_prop.sh with a parameter of 100 for each file.
 """
 import os, sys, math
 import re
 import linecache
 import shutil
 import itertools
-
 """Change the data_folder depending on where your files are"""
 data_folder = os.getcwd() 
 data_files = os.listdir(data_folder)
-
 """Reads each file given by data_folder and loops through to find the average bond length"""
 for file_name in data_files:
   if ".d3" in file_name:
